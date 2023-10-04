471bfe415d53aa4455334c2306bc2212eeafb839
Panni Belevski <pannibel@gmail.com>
2023-10-04 11:15:39 +0200

    wed

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import styles from "../../styles/page.module.scss";
 
 function Projects() {
+
   return (
     <main className={styles.main}>
       <header className={styles.header}>
@@ -15,8 +16,8 @@ function Projects() {
       <section className={styles.posts}>
         <div className={styles.projects}>
           <ul>
-            <li><Link href="/projects/pro1">PROJECT 01</Link></li>
-            <li><Link href="/projects/pro2">PROJECT 02</Link></li>
+            <li><Link href="https://chair.band/" target="_blank">CHAIR.BAND</Link></li>
+            <li><Link href="https://fruit-fest.netlify.app" target="_blank">RANGER FEST</Link></li>
             <li><Link href="/projects/pro3">PROJECT 03</Link></li>
             <li><Link href="/projects/pro2">PROJECT 04</Link></li>
             <li><Link href="/projects/pro1">PROJECT 05</Link></li>
